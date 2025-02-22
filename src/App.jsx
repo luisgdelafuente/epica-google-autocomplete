9a3c8018a490cfaf49d1ebc3b01220b408f5e64e
luisgdelafuente <34708827+luisgdelafuente@users.noreply.github.com>
2025-02-22 18:07:21 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -144,7 +144,7 @@ function App() {
             </svg>
           </div>
           <h1 className="text-3xl font-bold bg-gradient-to-r from-blue-400 via-purple-400 to-blue-400 bg-clip-text text-transparent">
-            Google Suggestions Amazing Tool
+            Google Suggestions Tool Branch2
           </h1>
         </div>
         
